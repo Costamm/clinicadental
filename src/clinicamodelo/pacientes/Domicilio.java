6b5c0b766d50e96d9461dee73beff8a852f4a158
Costamm <mateocosta@uade.edu.ar>
2026-05-05 13:57:16 -0300

    Saque Id domicilio

--- src/clinicamodelo/pacientes/Domicilio.java
+++ src/clinicamodelo/pacientes/Domicilio.java
@@ -1,7 +1,6 @@
 package clinicamodelo.pacientes;
 
 public class Domicilio {
-    private Long id;
     private String calle;
     private int numero;
     private String localidad;
@@ -10,16 +9,12 @@ public class Domicilio {
     public Domicilio() {
     }
 
-    public Domicilio(Long id, String calle, int numero, String localidad, String provincia) {
-        this.id = id;
+    public Domicilio(String calle, int numero, String localidad, String provincia) {
         this.calle = calle;
         this.numero = numero;
         this.localidad = localidad;
         this.provincia = provincia;
     }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
 
     public String getCalle() { return calle; }
     public void setCalle(String calle) { this.calle = calle; }
